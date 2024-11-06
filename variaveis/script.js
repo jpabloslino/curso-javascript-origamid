7729e9039a9575f81e602a5b1316ab7978a48dd8
jpabloslino <jpabloslino@gmail.com>
2024-11-06 14:34:45 -0300

    Começo do Curso de JavaSript da Origamid

---
+++ variaveis/script.js
@@ -0,0 +1,47 @@
+var nome = 'André';
+var idade = 28;
+var possuiFaculdade = true; // declarando variáveis linha por linha
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado*preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael',  // declarando variáveis "na mesma linha"
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir); // retorna undefined, variável existe, porém não contém valor
+
+// console.log(aindaNaoDefine);
+
+var comida;
+console.log(comida); // variável sem valor
+comida = 'Pizza';
+console.log(comida); // variável com o valor 'Pizza'
+
+var time = 'Vasco';
+console.log(time);
+
+time = 'Flamengo';
+console.log(time);
+
+// Exercício da aula
+
+var meuNome = 'João';
+var minhaIdade = 19;
+var minhaComidaFavorita;
+minhaComidaFavorita = 'Lasanha';
+var semVal1;
+var semVal2;
+var semVal3;
+var semVal4;
+var semVal5;
+
+console.log(meuNome, minhaIdade, minhaComidaFavorita, semVal1, semVal2, semVal3, semVal4, semVal5);
